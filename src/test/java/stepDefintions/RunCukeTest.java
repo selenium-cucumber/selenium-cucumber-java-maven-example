315fb05439611a56ea462cb3062138038e0e7c80
sameer49 <sameersawant222@gmail.com>
2017-09-19 21:48:30 +0530

    added example project

---
+++ src/test/java/stepDefintions/RunCukeTest.java
@@ -0,0 +1,17 @@
+package stepDefintions;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin = {"html:target/cucumberHtmlReport"},
+		//pretty:target/cucumber-json-report.json
+		features = "classpath:features",
+		glue = {"info.seleniumcucumber.stepdefinitions"}
+)
+
+public class RunCukeTest { 	
+}
