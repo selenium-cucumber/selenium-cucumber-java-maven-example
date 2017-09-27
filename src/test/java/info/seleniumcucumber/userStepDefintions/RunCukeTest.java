bb2d73d17130a7b54060fd629ffabf4ce3bc8eba
sameer49 <sameersawant222@gmail.com>
2017-09-27 15:08:24 +0530

    changed package name

---
+++ src/test/java/info/seleniumcucumber/userStepDefintions/RunCukeTest.java
@@ -0,0 +1,21 @@
+package info.seleniumcucumber.userStepDefintions;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin = 
+			{"pretty"},
+			//{"html:target/cucumberHtmlReport"},     //  for html result
+			//{pretty:target/cucumber-json-report.json},  // for json result
+		features = "classpath:features",
+		glue = {"info.seleniumcucumber.stepdefinitions",   // predefined step definitions package
+				"info.seleniumcucumber.userStepDefintions" // user step definitions package
+			   }
+)
+
+public class RunCukeTest { 	
+}
