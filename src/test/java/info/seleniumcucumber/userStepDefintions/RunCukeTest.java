7880190f2b102f6212b0a19210ded936c6cb893d
sameer49 <sameersawant222@gmail.com>
2017-09-28 13:55:30 +0530

    getting html result as well

--- src/test/java/info/seleniumcucumber/userStepDefintions/RunCukeTest.java
+++ src/test/java/info/seleniumcucumber/userStepDefintions/RunCukeTest.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		plugin = 
-			{"pretty"},
+			{"pretty", "html:target/cucumberHtmlReport"},
 			//{"html:target/cucumberHtmlReport"},     //  for html result
 			//{pretty:target/cucumber-json-report.json},  // for json result
 		features = "classpath:features",
