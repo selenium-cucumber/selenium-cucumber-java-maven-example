6f82bc6d6332bee6548e6ec7d6dd778d8286f9d4
sameer49 <sameersawant222@gmail.com>
2018-01-11 16:44:59 +0530

    added option to generate html/json results

--- src/test/java/info/seleniumcucumber/userStepDefintions/RunCukeTest.java
+++ src/test/java/info/seleniumcucumber/userStepDefintions/RunCukeTest.java
@@ -7,10 +7,11 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		plugin = 
-			{"pretty", "html:target/cucumberHtmlReport"},
-			//{"html:target/cucumberHtmlReport"},     //  for html result
-			//{pretty:target/cucumber-json-report.json},  // for json result
+		plugin = {
+			 "pretty", "html:target/cucumberHtmlReport",
+		     "html:target/cucumberHtmlReport",     //  for html result
+			 "pretty:target/cucumber-json-report.json"   // for json result
+		     },
 		features = "classpath:features",
 		glue = {"info.seleniumcucumber.stepdefinitions",   // predefined step definitions package
 				"info.seleniumcucumber.userStepDefintions" // user step definitions package
